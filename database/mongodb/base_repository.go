11df4540a7e696b8e9c18a25a19c1a291d75ea42
phongthan0402 <phongthan0402@gmail.com>
2025-07-05 15:30:14 +0700

    bổ sung hàm

--- database/mongodb/base_repository.go
+++ database/mongodb/base_repository.go
@@ -4,7 +4,9 @@ import (
 	"context"
 	"time"
 
+	"github.com/BeeTechHub/go-common/utils"
 	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
@@ -38,11 +40,17 @@ func (collection MongoCollectionWrapper) FindMany(sessContext mongo.SessionConte
 	return collection.Collection.Find(ctx, filter, opts...)
 }
 
-func (collection MongoCollectionWrapper) FindManyWithAggregation(sessContext mongo.SessionContext, pipeline []bson.M, opts ...*options.AggregateOptions) (*mongo.Cursor, error) {
+func (collection MongoCollectionWrapper) FindManyWithAggregation(sessContext mongo.SessionContext, pipeline []bson.M, records any, opts ...*options.AggregateOptions) error {
 	ctx, cancel := getOrCreateContext(sessContext, collection.Timeout)
 	defer cancel()
 
-	return collection.Collection.Aggregate(ctx, pipeline, opts...)
+	cursor, err := collection.Collection.Aggregate(ctx, pipeline, opts...)
+	if err != nil {
+		return err
+	}
+	defer cursor.Close(context.TODO())
+
+	return cursor.All(context.TODO(), records)
 }
 
 func (collection MongoCollectionWrapper) UpdateOne(sessContext mongo.SessionContext, filter bson.M, update bson.M, opts ...*options.UpdateOptions) (*mongo.UpdateResult, error) {
@@ -85,4 +93,95 @@ func (collection MongoCollectionWrapper) BulkWrite(sessContext mongo.SessionCont
 	defer cancel()
 
 	return collection.Collection.BulkWrite(ctx, models, opts...)
+}
+
+func (collection MongoCollectionWrapper) FindOneById(sessContext mongo.SessionContext, record any, id primitive.ObjectID) error {
+	ctx, cancel := getOrCreateContext(sessContext, collection.Timeout)
+	defer cancel()
+
+	return collection.Collection.FindOne(ctx, bson.M{"_id": id}).Decode(record)
+}
+
+func (collection MongoCollectionWrapper) DeleteOneById(sessContext mongo.SessionContext, id primitive.ObjectID) error {
+	ctx, cancel := getOrCreateContext(sessContext, collection.Timeout)
+	defer cancel()
+
+	_, err := collection.Collection.DeleteMany(ctx, bson.M{"_id": id})
+	return err
+}
+
+func (collection MongoCollectionWrapper) FindManyByIds(sessContext mongo.SessionContext, records any, ids []primitive.ObjectID, opts ...*options.FindOptions) error {
+	ctx, cancel := getOrCreateContext(sessContext, collection.Timeout)
+	defer cancel()
+
+	cursor, err := collection.Collection.Find(ctx, bson.M{"_id": bson.M{"$in": ids}}, opts...)
+	if err != nil {
+		return err
+	}
+	defer cursor.Close(context.TODO())
+
+	return cursor.All(context.TODO(), records)
+}
+
+func (collection MongoCollectionWrapper) DeleteManyByIds(sessContext mongo.SessionContext, ids []primitive.ObjectID) error {
+	ctx, cancel := getOrCreateContext(sessContext, collection.Timeout)
+	defer cancel()
+
+	_, err := collection.Collection.DeleteMany(ctx, bson.M{"_id": bson.M{"$in": ids}})
+	return err
+}
+
+func (collection MongoCollectionWrapper) FindPaginated(sessContext mongo.SessionContext, records any, page int64, size int64, filter bson.M, sortParams bson.D, pipe ...bson.M) (int64, error) {
+	ctx, cancel := getOrCreateContext(sessContext, collection.Timeout)
+	defer cancel()
+
+	matchFilter := bson.M{
+		"$match": filter,
+	}
+
+	if len(sortParams) == 0 {
+		sortParams = bson.D{
+			{"_id", 1},
+		}
+	}
+
+	sort := bson.M{
+		"$sort": sortParams,
+	}
+
+	limit := bson.M{
+		"$limit": size,
+	}
+
+	skip := bson.M{
+		"$skip": utils.CalculatePaginatedSkip(page, size),
+	}
+
+	pipeline := []bson.M{matchFilter}
+
+	if len(pipe) > 0 {
+		pipeline = append(pipeline, pipe...)
+	}
+
+	pipeline = append(pipeline, sort, limit, skip)
+
+	count, err := collection.Collection.CountDocuments(ctx, filter)
+	if err != nil {
+		return 0, err
+	} else if count == 0 {
+		return 0, nil
+	}
+
+	cursor, err := collection.Collection.Aggregate(ctx, pipeline)
+	if err != nil {
+		return 0, err
+	}
+	defer cursor.Close(context.TODO())
+
+	err = cursor.All(context.TODO(), records)
+	if err != nil {
+		return 0, err
+	}
+
+	return count, err
 }
